9b661dc779ecf9017b8c01fd5d6dff0d0e90a2e2
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-04-28 17:21:59 +0100

    wip

--- views/welcome.blade.php
+++ views/welcome.blade.php
@@ -14,8 +14,8 @@
 
     <div class="min-w-screen min-h-screen bg-gray-100 flex  justify-center bg-gray-100 font-sans overflow-hidden">
         <div class="w-full lg:w-5/6">
+            <h1 class="mx-auto text-5xl pt-12 pb-2">Routes (1)</h1>
             <div class="bg-white shadow-md rounded my-6">
-                <h1 class="mx-6 text-5xl pb-2">Routes</h1>
                 <table class="min-w-max w-full table-auto">
                     <thead>
                     <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
