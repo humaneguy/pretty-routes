95d80155a711786af00b51099e9107a658ec9dbf
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-04-28 19:53:36 +0100

    wip

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -5,15 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('hello', [RootzController::class, 'greet']);
 
-
-Route::post('wait', function () {
-    return 'Posted';
-});
-
-Route::delete('dont-stress-me', function () {
-    return 'Posted';
-});
-
 Route::get('routes', [RootzController::class, 'show'])
     ->name('rootz.show')
     ->middleware(config('rootz.middlewares'));
