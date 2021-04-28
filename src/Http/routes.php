bb41992601d638d970455659b7af105733ee8fcf
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-04-28 17:30:57 +0100

    wip

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('hello', [RootzController::class, 'greet']);
 
+
+Route::post('wait', function () {
+    return 'Posted';
+});
+
 Route::get('routes', [RootzController::class, 'show'])
     ->name('rootz.show')
     ->middleware(config('rootz.middlewares'));
