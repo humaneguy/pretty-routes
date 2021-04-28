8fdec61d93c9c06db28b40ebb7fc7d19d3cece20
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-04-28 16:47:39 +0100

    wip

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,4 +2,9 @@
 
 use GabrielFemi\Rootz\PrettyRoutesController;
 use Illuminate\Support\Facades\Route;
+
 Route::get('hello', [PrettyRoutesController::class, 'greet']);
+
+Route::get(config('pretty-routes.url'), 'PrettyRoutes\PrettyRoutesController@show')
+    ->name('pretty-routes.show')
+    ->middleware(config('pretty-routes.middlewares'));
